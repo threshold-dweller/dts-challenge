76ed2dcb8ba7d460cde146d5bbb66a04f6f66ee6
threshold-dweller <ryrobedgar@gmail.com>
2026-05-23 18:30:25 +0100

    mock TaskService in web layer integration test

---
+++ backend/src/integrationTest/java/uk/gov/hmcts/reform/dev/controllers/GetWelcomeTest.java
@@ -0,0 +1,32 @@
+package uk.gov.hmcts.reform.dev.controllers;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import uk.gov.hmcts.reform.dev.services.TaskService;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+class GetWelcomeTest {
+
+    @Autowired
+    private transient MockMvc mockMvc;
+
+    @MockitoBean
+    private TaskService taskService;
+
+    @DisplayName("Should welcome upon root request with 200 response code")
+    @Test
+    void welcomeRootEndpoint() throws Exception {
+        MvcResult response = mockMvc.perform(get("/")).andExpect(status().isOk()).andReturn();
+
+        assertThat(response.getResponse().getContentAsString()).startsWith("Welcome");
+    }
+}
